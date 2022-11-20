054391a92d844ff62b23589f38c6e5766148ee66
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-11-20 17:58:09 +0200

    Update title

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,14 @@
 // Interacting with the weather API
+const API_KEY = '';
+
+const getCity = async (city) => {
+    const base = '';
+    const query = `?apikey=${API_KEY}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getCity('manchester');
