52e73ec06f61584f6d8af8328440ba3fd22905e8
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-11-21 19:46:52 +0200

    Add icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-    
+
     const { cityDets, weather } = data;
 
     // update details template
@@ -16,7 +18,18 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    // update the day/night icon images
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+    time.setAttribute('src', timeSrc);
+
     // remove the d-none class if present
+
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
