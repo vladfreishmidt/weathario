603cf80092c1863db93982ce0f6c77ad9944454f
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-11-21 16:25:22 +0200

    Add getWeather

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,16 @@
 // Interacting with the weather API
 const API_KEY = '';
 
+const getWeather = async (id) => {
+    const base = '';
+    const query = `${id}?apikey=${API_KEY}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
 const getCity = async (city) => {
     const base = '';
     const query = `?apikey=${API_KEY}&q=${city}`;
@@ -8,7 +18,11 @@ const getCity = async (city) => {
     const response = await fetch(base + query);
     const data = await response.json();
 
-    console.log(data);
-}
+    return data[0];
+};
 
-getCity('manchester');
+getCity('manchester').then(data => {
+    return getWeather(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
