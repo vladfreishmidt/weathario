e26c145b8be4d9ff5292ebfa15c7a1502d592752
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-11-21 19:25:01 +0200

    Add render UI on search submit

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,32 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    
+    const { cityDets, weather } = data;
+
+    // update details template
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+    // remove the d-none class if present
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+}
 
 const updateCity = async (city) => {
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets,
-        weather
-    }
+    return { cityDets, weather }
 };
 
 cityForm.addEventListener('submit', e => {
@@ -19,6 +38,6 @@ cityForm.addEventListener('submit', e => {
 
     // update UI with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
 });
