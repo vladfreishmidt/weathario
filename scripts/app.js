69b0e7b85b7d3193629ad36413e97871249ca0af
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2022-11-21 16:46:15 +0200

    Update UI with new city

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
-// DOM manipulations
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return {
+        cityDets,
+        weather
+    }
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update UI with new city
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+});
